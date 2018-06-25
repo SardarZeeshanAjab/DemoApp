012a863bd5aef626054ee26ac883432e3e3acf55
SardarZeeshanAjab <khanshani306@gmail.com>
2018-06-25 18:28:30 +0500

    Revert "themeCustomization"

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -17,11 +17,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func loginBtn(_ sender: Any) {
-               getDetail()
+    
     }
-    func getDetail(){
-        print("this is a demo app")
-    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
